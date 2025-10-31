ba8b15ddb26ffb95b2f63e5c5131d76a341ff5b2
Abdullah <abdullahyousuf1836@gmail.com>
2025-10-31 14:58:10 +0500

    completed task3

--- lab8/task3.cpp
+++ lab8/task3.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+using namespace std;
 
+struct node {
+    int data;
+    node* left;
+    node* right;
+    node(int value) {
+        data = value;
+        left = right = nullptr;
+    }
+};
+node* createnode(int data) {
+    return new node(data);
+}
+bool isfulltree(node* root) {
+    if (root == nullptr) {
+        return true; 
+    }
+    if (root->left == nullptr && root->right == nullptr) {
+        return true;
+    }
+    if (root->left != nullptr && root->right != nullptr) {
+        return isfulltree(root->left) && isfulltree(root->right);
+    }
+    return false;
+}
+int main() {
+    node* root = createnode(1);
+    root->left = createnode(2);
+    root->right = createnode(3);
+    root->left->left = createnode(4);
+    root->left->right = createnode(5);
+
+    if (isfulltree(root)) {
+        cout << "it is a full binary tree." << endl;
+    } else {
+        cout << "it is not a full binary tree." << endl;
+    }
+    return 0;
+}
