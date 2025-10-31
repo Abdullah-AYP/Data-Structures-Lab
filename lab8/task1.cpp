e1ed2bb4c0bae9a0a5d322915b28942cfec02b2b
Abdullah <abdullahyousuf1836@gmail.com>
2025-10-31 14:51:24 +0500

    completed task1

--- lab8/task1.cpp
+++ lab8/task1.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
 
+struct node {
+    int data;
+    node* left;
+    node* right;
+
+    node(int value) {
+        data = value;
+        left = right = nullptr;
+    }
+};
+node* createnode(int data) {
+    return new node(data);
+}
+void displaytours(node* root) {
+    if (root == nullptr) {
+        return;
+    }
+    displaytours(root->left);
+    cout << "tour package id: " << root->data << endl;
+    displaytours(root->right);
+}
+int main() {
+    node* root = createnode(1);
+    root->left = createnode(2);
+    root->right = createnode(3);
+    root->left->left = createnode(4);
+    root->left->right = createnode(5);
+
+    cout << "hierarchy of tour packages:" << endl;
+    displaytours(root);
+
+    return 0;
+}
